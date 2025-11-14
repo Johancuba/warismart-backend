8dd8809136963d2d29ca187f927856335f93565f
Johancuba <168486887+Johancuba@users.noreply.github.com>
2025-11-13 20:37:34 -0500

    Delete WariSmart/News directory

--- WariSmart/News/Interfaces/REST/Resources/FavoriteSourceResource.cs
+++
@@ -1,9 +0,0 @@
-namespace CatchUpPlatform.API.News.Interfaces.REST.Resources;
-
-/// <summary>
-/// Represents the data provided by the server about a favorite source. 
-/// </summary>
-/// <param name="Id">The server-generated ID</param>
-/// <param name="NewsApiKey">The News API Key</param>
-/// <param name="SourceId">The Source ID</param>
-public record FavoriteSourceResource(int Id, string NewsApiKey, string SourceId);
